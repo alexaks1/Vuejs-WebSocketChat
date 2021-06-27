7828152acf99ac024d869af91f115bc54029b19b
Vadim Kholoshnia <vadim.kholoshnia@tune-it.ru>
2021-06-28 02:21:08 +0300

    Update user model

--- auth/src/main/java/ru/project/auth/controllers/userController/dto/UserResponseDTO.java
+++ auth/src/main/java/ru/project/auth/controllers/userController/dto/UserResponseDTO.java
@@ -2,10 +2,15 @@ package ru.project.auth.controllers.userController.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.project.auth.model.entity.Role;
 
 
 @AllArgsConstructor
 @Data
 public class UserResponseDTO {
-    private final DetailsDTO detailsDTO;
+    private final Number id;
+    private final String login;
+    private final String firstName;
+    private final String lastName;
+    private final Role role;
 }
