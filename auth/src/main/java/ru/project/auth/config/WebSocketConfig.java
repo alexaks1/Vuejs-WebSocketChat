8a5b55d6b6504c22cd6acfb4c69c24ca8218f174
Vadim Kholoshnia <vadim.kholoshnia@tune-it.ru>
2021-06-28 03:46:48 +0300

    Update cors

--- auth/src/main/java/ru/project/auth/config/WebSocketConfig.java
+++ auth/src/main/java/ru/project/auth/config/WebSocketConfig.java
@@ -27,7 +27,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("*")
                 .withSockJS();
     }
 
