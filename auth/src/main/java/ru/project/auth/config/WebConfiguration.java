6acc31590d9d13459c71c272791c126b5b479f01
Vadim Kholoshnia <vadim.kholoshnia@tune-it.ru>
2021-06-28 03:49:48 +0300

    Update cors

--- auth/src/main/java/ru/project/auth/config/WebConfiguration.java
+++ auth/src/main/java/ru/project/auth/config/WebConfiguration.java
@@ -1,7 +1,10 @@
 package ru.project.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -37,5 +40,15 @@ public class WebConfiguration extends OncePerRequestFilter {
         response.setHeader("Access-Control-Allow-Headers",
                 "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
         filterChain.doFilter(request, response);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
