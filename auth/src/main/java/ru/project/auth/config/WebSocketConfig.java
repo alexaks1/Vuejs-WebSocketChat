3d6052e1f7ac48ed92f45bb23f608aadd84e12e1
Vadim Kholoshnia <vadim.kholoshnia@tune-it.ru>
2021-06-28 04:34:35 +0300

    Update cors

--- auth/src/main/java/ru/project/auth/config/WebSocketConfig.java
+++ auth/src/main/java/ru/project/auth/config/WebSocketConfig.java
@@ -3,6 +3,7 @@ package ru.project.auth.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.messaging.converter.DefaultContentTypeResolver;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.messaging.converter.MessageConverter;
@@ -14,6 +15,7 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.Collections;
 import java.util.List;
 
 
@@ -38,11 +40,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public boolean configureMessageConverters(List<MessageConverter> messageConverters) {
-        DefaultContentTypeResolver resolver = new DefaultContentTypeResolver();
-        resolver.setDefaultMimeType(MimeTypeUtils.APPLICATION_JSON);
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
         converter.setObjectMapper(new ObjectMapper());
-        converter.setContentTypeResolver(resolver);
         messageConverters.add(converter);
         return false;
     }
